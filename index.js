8b8889eb1f52d4d24126e728ce1cc2603ee033cd
Chrysa Lazaridou <chrysanthi.lazaridou@gmail.com>
2021-01-28 17:24:02 +0200

    fisrt commit

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+exports.__esModule = true;
+// Exercise 1: Write a tuple of string and number;
+var tup;
+// Exercise 2: Write a pure arrow function that returns a number.
+var ex2 = function (num) { return num; };
+// Exercise 3: Write a function that returns a string.
+function ex3(str) {
+    return str;
+}
+;
+ex3("Hello!");
+// Exercise 4: Write a function that returns a void.
+function ex4(num4) {
+    console.log(num4);
+}
+// Exercise 5: Write an interface of the an object. const person = {name:
+// 'costas', age: 40, address: { str: 'lala 40', city: 'Thessaloniki',
+// post: 55132}}
+// Exercise 6: Wtite a class Person. it takes name and age and has a method
+// that walk that prints walk. Make an instace from that class
+// Exercises
+var message;
+message = "abc";
+//let endsWith = (<string>message).endsWith("c");
+// Exercise 1: In index.ts add type assercion to the variable message;
+// Exercise 2: In index.ts create a function printDetails that takes an
+// object with 2 properties as an argument, firstName and lastName and
+// prints out the concatenation of these 2. Add an interface to the
+// argument.
+// Exercise 3: In index.ts create class User. It should take 3 arguments
+// that are strings, firstName, lastName and age. It should have a method
+// fullName that prints out the concatenation of both. Make lastName to
+// have a default value of 'Patel' and the age optional. Also make it that
+// you can't chage firstName after initialization.
+// Exercise 4: Write a function that returns a void.
+// Exercise 5: Write an interface of the an object. const person = {name:
+// 'costas', age: 40, address: { str: 'lala 40', city: 'Thessaloniki',
+// post: 55132}}
+// Exercise 6: Wtite a class Person. it takes name and age and has a method
+// that walk that prints walk. Make an instace from that class
---
+++ index.ts
@@ -0,0 +1,62 @@
+export{}
+
+// Exercise 1: Write a tuple of string and number;
+
+let tup:[string, number];
+
+// Exercise 2: Write a pure arrow function that returns a number.
+
+let ex2 = (num:number):number => num ;
+
+// Exercise 3: Write a function that returns a string.
+
+function ex3(str:string){
+  return str
+};
+
+ex3("Hello!");
+// Exercise 4: Write a function that returns a void.
+
+function ex4(num4:number){
+    console.log(num4)
+}
+
+// Exercise 5: Write an interface of the an object. const person = {name:
+// 'costas', age: 40, address: { str: 'lala 40', city: 'Thessaloniki',
+// post: 55132}}
+
+// Exercise 6: Wtite a class Person. it takes name and age and has a method
+// that walk that prints walk. Make an instace from that class
+
+// Exercises
+
+let message;
+message = "abc";
+
+
+//let endsWith = (<string>message).endsWith("c");
+
+
+
+// Exercise 1: In index.ts add type assercion to the variable message;
+
+// Exercise 2: In index.ts create a function printDetails that takes an
+// object with 2 properties as an argument, firstName and lastName and
+// prints out the concatenation of these 2. Add an interface to the
+// argument.
+
+// Exercise 3: In index.ts create class User. It should take 3 arguments
+// that are strings, firstName, lastName and age. It should have a method
+// fullName that prints out the concatenation of both. Make lastName to
+// have a default value of 'Patel' and the age optional. Also make it that
+// you can't chage firstName after initialization.
+
+// Exercise 4: Write a function that returns a void.
+
+// Exercise 5: Write an interface of the an object. const person = {name:
+// 'costas', age: 40, address: { str: 'lala 40', city: 'Thessaloniki',
+// post: 55132}}
+
+// Exercise 6: Wtite a class Person. it takes name and age and has a method
+// that walk that prints walk. Make an instace from that class
+
